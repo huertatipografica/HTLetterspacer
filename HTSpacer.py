66009010c7e8d72b32bf33fcd99fb8a028f19aef
schriftgestalt <georg.seifert@mac.com>
2016-08-04 22:43:30 +0200

    divide code into script and lib
    this needs more work and might not work in all cases, yet...

---
+++ HTSpacer.py
@@ -0,0 +1,263 @@
+#MenuTitle: HT Spacer
+#
+# LetterSpacer, an auto-spacing tool
+# Copyright (C) 2009 - 2016, The LetterSpacer Project Authors
+#
+# This script can only be used with permission
+# 
+# Version 1.0.0
+
+# Basic config
+# if window = True, scripts run with a UI
+window = False
+# Tabular: if True doesn't change glyph widths // It is automatic for .tf or .tosf glyphs
+tabVersion = False
+
+# ADVANCED CONFIG
+# mark color
+color = 1
+# creates -areas- glyph, with a drawing of the white space in letter. Requires robofab.
+createProofGlyph = False
+
+
+# program
+#  Dependencies
+import GlyphsApp
+import math
+import numpy as np
+
+import htSpacerLib
+reload(htSpacerLib)
+
+if createProofGlyph:
+	from objectsGS import *
+	f = CurrentFont()
+
+#  Functions
+
+# file
+def fileLocation():
+	path = Glyphs.font.filepath.split('/')
+	del path[len(path) - 1]
+	output = ''
+	for f in path:
+		output += f + '/'
+	return output
+
+
+def config():
+	path = fileLocation()
+	filename = Glyphs.font.filepath.split('/')
+	filename = filename[-1].split('.')
+	filename = filename[0]
+	filename = path + filename + "_autospace.py"
+	array = []
+	with open(filename) as f:
+		for line in f:
+			if line[0] != '#' and len(line) > 5:
+				newline = line.split(",")
+				del newline[-1]
+				newline[3] = float(newline[3])
+				array.append(newline)
+	return array
+
+
+class HTSpacerScript(object):
+
+	def __init__(self):
+		
+		self.engine = htSpacerLib.HTSpacerLib()
+		
+		self.font = Glyphs.font
+		selectedLayers = Glyphs.font.selectedLayers
+		if len(selectedLayers) == 0:
+			self.output += "Nothing selected\n"
+			return
+		self.mySelection = list(set(selectedLayers))
+		self.output = ''
+		self.layerID = self.mySelection[0].associatedMasterId
+		self.master = self.font.masters[self.layerID]
+		
+		
+		self.engine.angle = self.master.italicAngle
+		self.engine.xHeight = self.master.xHeight
+		self.engine.mline = self.master.xHeight / 2
+
+		self.getParams()
+		self.engine.paramDepth = int(self.engine.paramDepth)
+		self.engine.paramOver = int(self.engine.paramOver)
+
+		self.engine.tab = False
+		self.engine.LSB = True
+		self.engine.RSB = True
+
+		if window:
+			self.window()
+		else:
+			for layer in self.mySelection:
+				self.setG(layer)
+				self.engine.spaceMain(layer, self.referenceLayer)
+
+	def getParams(self):
+		customArea = self.master.customParameters["paramArea"]
+		if customArea:
+			self.engine.paramArea = customArea
+			self.output += 'Using master custom parameter, paramArea: ' + str(self.engine.paramArea) + "\n"
+		else:
+			self.output += 'Using DEFAULT PARAMETERS, paramArea: ' + str(self.engine.paramArea) + "\n"
+
+		customDepth = self.master.customParameters["paramDepth"]
+		if customDepth:
+			self.output += 'Using master custom parameter, paramDepth: ' + str(self.engine.paramDepth) + "\n"
+		else:
+			self.output += 'Using DEFAULT PARAMETERS, paramDepth: ' + str(self.engine.paramDepth) + "\n"
+
+		customOver = self.master.customParameters["paramOver"]
+		if customOver:
+			self.engine.paramOver = customOver
+			self.output += 'Using master custom parameter, paramOver: ' + str(self.engine.paramOver) + "\n"
+		else:
+			self.output += 'Using DEFAULT PARAMETERS, paramOver: ' + str(self.engine.paramOver) + "\n"
+
+	def window(self):
+		self.w = vanilla.FloatingWindow((250, 200), "AutoSpacer", minSize=(225, 200), maxSize=(225, 200), autosaveName="com.ht.spacer")
+		self.w.text_3 = vanilla.TextBox((210, 25, -170, 14), "%", sizeStyle='small')
+		self.w.text_4 = vanilla.TextBox((15, 50, 100, 14), "Area", sizeStyle='small')
+		self.w.text_4b = vanilla.TextBox((140, 50, 50, 14), self.paramArea, sizeStyle='small')
+		self.w.text_5 = vanilla.TextBox((15, 75, 100, 14), "Depth", sizeStyle='small')
+		self.w.text_5b = vanilla.TextBox((140, 75, 50, 14), self.paramDepth, sizeStyle='small')
+		self.w.text_6 = vanilla.TextBox((15, 100, 100, 14), "Overshot", sizeStyle='small')
+		self.w.text_6b = vanilla.TextBox((140, 100, 50, 14), self.paramOver, sizeStyle='small')
+		self.w.LSB = vanilla.CheckBox((15, 25, 40, 18), "LSB", value=True, sizeStyle='small', callback=self.SavePreferences)
+		self.w.RSB = vanilla.CheckBox((15 + 45, 25, 40, 18), "RSB", value=True, sizeStyle='small', callback=self.SavePreferences)
+		self.w.tab = vanilla.CheckBox((15 + 45 + 45, 25, 60, 18), "Tabular", value=False, sizeStyle='small', callback=self.SavePreferences)
+		self.w.ancho = vanilla.EditText((170, 25, 40, 18), self.mySelection[0].width, sizeStyle='small')
+		self.w.area = vanilla.EditText((170, 50 - 3, 40, 18), "430", sizeStyle='small')
+		self.w.prof = vanilla.EditText((170, 75 - 3, 40, 18), "20", sizeStyle='small')
+		self.w.ex = vanilla.EditText((170, 100 - 3, 40, 18), "0", sizeStyle='small')
+
+		self.w.runButton = vanilla.Button((15, 125, 90, 25), "Apply", sizeStyle='small', callback=self.dialogCallback)
+
+		self.w.setDefaultButton(self.w.runButton)
+
+		if not self.LoadPreferences():
+			print "Error: Could not load preferences. Will resort to defaults."
+
+		self.w.open()
+
+	def dialogCallback(self, sender):
+		self.engine.paramArea = int(self.w.area.get())
+		self.engine.paramDepth = int(self.w.prof.get())
+		self.engine.paramOver = int(self.w.ex.get())
+		self.engine.tab = self.w.tab.get()
+		self.engine.LSB = self.w.LSB.get()
+		self.engine.RSB = self.w.RSB.get()
+		self.engine.ancho = float(self.w.ancho.get())
+		self.spaceMain()
+		
+		if not self.SavePreferences(self):
+			print "Note: Couldn't save preferences."
+
+	def SavePreferences(self, sender):
+		try:
+			Glyphs.defaults["com.ht.spacer.LSB"] = self.w.LSB.get()
+			Glyphs.defaults["com.ht.spacer.RSB"] = self.w.RSB.get()
+			Glyphs.defaults["com.ht.spacer.tab"] = self.w.tab.get()
+			Glyphs.defaults["com.ht.spacer.area"] = self.w.area.get()
+			Glyphs.defaults["com.ht.spacer.depth"] = self.w.prof.get()
+			Glyphs.defaults["com.ht.spacer.over"] = self.w.ex.get()
+		except:
+			return False
+
+		return True
+
+	def LoadPreferences(self):
+		try:
+			self.w.LSB.set(Glyphs.defaults["com.ht.spacer.LSB"])
+			self.w.RSB.set(Glyphs.defaults["com.ht.spacer.RSB"])
+			self.w.tab.set(Glyphs.defaults["com.ht.spacer.tab"])
+			self.w.area.set(Glyphs.defaults["com.ht.spacer.area"])
+			self.w.prof.set(Glyphs.defaults["com.ht.spacer.depth"])
+			self.w.ex.set(Glyphs.defaults["com.ht.spacer.over"])
+		except:
+			return False
+
+		return True
+
+
+	# progress bar
+	def progressBar(self):
+		self.p = vanilla.Window((300, 40))
+		self.p.bar = vanilla.ProgressBar((10, 10, -10, 16))
+		self.p.open()
+		self.p.bar.set(0)
+		self.wunit = 100.000 / len(self.mySelection)
+
+	def findException(self):
+		exception = False
+		items = []
+		for item in config():
+			if self.script == item[0] or item[0] == '*':
+				if self.category == item[1] or item[1] == '*':
+					if self.subCategory == item[2] or item[2] == '*':
+						items.append(item)
+		if len(items) > 0:
+			exception = items[0]
+			for item in items:
+				if item[5] in self.glyph.name:
+					exception = item
+		return exception
+
+	def setG(self, layer):
+		self.output = '\\' + layer.parent.name + '\\\n' + self.output
+		
+		self.layerID = layer.associatedMasterId
+		self.master = self.font.masters[self.layerID]
+		
+		self.glyph = layer.parent
+		self.layer = layer
+		self.category = layer.parent.category
+		self.subCategory = layer.parent.subCategory
+		self.script = layer.parent.script
+		self.engine.reference = self.glyph.name
+		self.exception = self.findException()
+
+		if (self.exception):
+			self.engine.factor = self.exception[3]
+		
+		self.engine.minYref = 0
+		self.engine.maxYref = 0
+		self.engine.minYg = 0
+		self.engine.maxYg = 0
+
+		self.engine.newL = layer.LSB
+		self.engine.newR = layer.RSB
+		self.engine.newWidth = False
+
+		self.setReference()
+
+		# get reference glyph, if exists
+		reference = self.reference
+		# check reference layer existance and contours
+		if self.font.glyphs[reference]:
+			self.referenceLayer = self.font.glyphs[reference].layers[self.layerID]
+			if len(self.referenceLayer.paths) < 1:
+				self.output += "WARNING: The reference glyph declared (" + self.reference + ") doesn't have contours. Glyph " + self.layer.parent.name + " was spaced uses its own vertical range.\n"
+				self.referenceLayer = self.layer
+		else:
+			self.referenceLayer = self.layer
+			self.output += "WARNING: The reference glyph declared (" + self.reference + ") doesn't exist. Glyph " + self.layer.parent.name + " was spaced uses its own vertical range.\n"
+	
+	def setReference(self):
+		if (self.exception):
+			item = self.exception[4]
+			if item != '*':
+				self.reference = item
+	
+	def spaceMain(self):
+		for layer in self.mySelection:
+			self.setG(layer)
+			self.engine.spaceMain(layer)
+
+
+HTSpacerScript()
---
+++ htSpacerLib.py
@@ -0,0 +1,403 @@
+#
+# LetterSpacer, an auto-spacing tool
+# Copyright (C) 2009 - 2016, The LetterSpacer Project Authors
+#
+# This script can only be used with permission
+# 
+# Version 1.0.0
+
+# Default parameters
+paramArea = 400  # white area in thousand units
+paramDepth = 15  # depth in open counterforms, from extreme points.
+paramOver = 0    # overshoot in spacing vertical range
+
+# ADVANCED CONFIG
+# mark color
+color = 1
+# creates -areas- glyph, with a drawing of the white space in letter. Requires robofab.
+createProofGlyph = False
+# frequency of vertical measuring. Higher values are faster but less accurate
+paramFreq = 10
+
+
+# program
+#  Dependencies
+import GlyphsApp
+import math
+import numpy as np
+from Foundation import NSMinY, NSMaxY, NSMakePoint
+if createProofGlyph:
+	from objectsGS import *
+	f = CurrentFont()
+
+#  Functions
+
+def setSidebearings(layer, newL, newR, width, color):
+	layer.LSB = newL
+	layer.RSB = newR
+
+	# ajusta el error de calculo tabular
+	if width:
+		layer.width = width
+
+	if color:
+		layer.parent.color = color
+
+
+# shape calculations
+def rectCateto(angle, cat):
+	angle = math.radians(angle)
+	result = cat * (math.tan(angle))
+	#result = round(result)
+	return result
+
+
+# point list area
+def area(points):
+	s = 0
+	for ii in np.arange(len(points)) - 1:
+		s = s + (points[ii][0] * points[ii + 1][1] - points[ii + 1][0] * points[ii][1])
+	return abs(s) * 0.5
+
+
+# get margins in Glyphs
+def getMargins(layer, y):
+	startPoint = NSMakePoint(-1000, y)
+	endPoint = NSMakePoint(3000, y)
+
+	result = layer.calculateIntersectionsStartPoint_endPoint_(startPoint, endPoint)
+	
+	count = len(result)
+	if (count > 2):
+		left = 1
+		right = count - 2
+		margins = (result[left].pointValue().x, result[right].pointValue().x)
+	else:
+		margins = (None, None)
+	return margins
+
+
+# a list of margins
+def marginList(layer, bottom, top):
+	y = bottom
+	listL = []
+	listR = []
+	# works over glyph copy
+	cleanLayer = layer.copyDecomposedLayer()
+	while y <= top:
+		margins = getMargins(cleanLayer, y)
+		lpos = margins[0]
+		rpos = margins[1]
+		listL.append([lpos, y])
+		listR.append([rpos, y])
+		y += paramFreq
+	lista = [listL, listR]
+	return lista
+
+
+def marginsZone(margins, bottom, top):
+	marginsNew = [[], []]
+	for index in range(len(margins[0])):
+		if margins[0][index][1] >= bottom and margins[0][index][1] <= top:
+			marginsNew[0].append(margins[0][index])
+			marginsNew[1].append(margins[1][index])
+	return marginsNew
+
+
+# sets depth for each point in list
+# left
+def setDepthInListL(lista, depth, puntosExtremos):
+	list = []
+	for p in lista:
+		# si es nulo lo pone a la profundiad
+		if p[0] is not None:
+			x = p[0]
+			if p[0] > (puntosExtremos[0][0] + depth):
+				x = puntosExtremos[0][0] + depth
+		else:
+			x = puntosExtremos[0][0] + depth
+		list.append([x, p[1]])
+	return list
+
+
+# right
+def setDepthInListR(lista, depth, puntosExtremos):
+	list = []
+	for p in lista:
+		if p[0] is not None:
+			x = p[0]
+			if p[0] < (puntosExtremos[1][0] - depth):
+				x = puntosExtremos[1][0] - depth
+		else:
+			x = puntosExtremos[1][0] - depth
+		list.append([x, p[1]])
+	return list
+
+
+# creates proof glyph
+def createAreasGlyph(font, origenLayer, layerIndex, margins):
+	from robofab.pens.marginPen import MarginPen
+	if 'areas' in f:
+		areaLayer = font.glyphs['areas'].layers[layerIndex]
+		# f.removeGlyph('areas')
+		#del font.glyphs["areas"]
+		# removeSegment(index)
+
+		for i in range(len(areaLayer.paths))[::-1]:
+			del areaLayer.paths[i]
+	else:
+		f.newGlyph("areas")
+	origen = f[origenLayer.parent.name]
+
+	drawArea(origen, f['areas'], margins[0])
+	drawArea(origen, f['areas'], margins[1])
+
+
+# draw outlines on areas glyph
+def drawArea(origen, destination, puntos):
+	destination.width = origen.width
+	# pen
+	pen = destination.getPen()
+
+	# Tell the pen to draw things
+	pen.moveTo(puntos[0])
+	for p in puntos:
+		pen.lineTo(p)
+
+	# Done drawing: close the path
+	pen.closePath()
+	print destination
+	# destination.update()
+
+
+class HTSpacerLib(object):
+
+	def __init__(self):
+		self.paramArea = paramArea
+		self.paramDepth = paramDepth
+		self.paramOver = paramOver
+		self.tabVersion = False
+
+	# calculates distance over and below
+	def excedente(self):
+		valorExcedente = self.xHeight * self.paramOver / 100
+		return valorExcedente
+
+	def maxPoints(self, points, minY, maxY):
+		right = -10000
+		left = 10000
+		for p in points:
+			if p[1] >= minY and p[1] <= maxY:
+				if p[0] > right and p[0] is not None:
+					right = p[0]
+					righty = p[1]
+				if p[0] < left and p[0] is not None:
+					left = p[0]
+					lefty = p[1]
+		return ((left, lefty), (right, righty))
+
+	def processMargins(self, margins):
+		# deSlant if is italic
+		margins = self.deSlant(margins)
+		# get extremes
+		puntosExtremos = self.maxPoints(margins[0] + margins[1], self.minYref, self.maxYref)
+
+		# set depth
+		margins = self.setDepth(margins[0], margins[1], puntosExtremos)
+		# close open counterforms at 45 degrees
+		margins = self.diagonize(margins[0], margins[1])
+		margins = self.closeOpenCounters(margins[0], margins[1], puntosExtremos)
+		margins = self.slant(margins)
+		return margins[0], margins[1]
+
+	# process lists with depth, proportional to xheight
+	def setDepth(self, marginsL, marginsR, puntosExtremos):
+		depth = self.xHeight * self.paramDepth / 100
+		marginsL = setDepthInListL(marginsL, depth, puntosExtremos)
+		marginsR = setDepthInListR(marginsR, depth, puntosExtremos)
+		return marginsL, marginsR
+
+	# close counterforms at 45 degrees
+	def diagonize(self, marginsL, marginsR):
+		total = len(marginsL) - 1
+
+		valueFreq = paramFreq * 1.5
+		for index in range(total):
+			# left
+			actualPoint = marginsL[index]
+			nextPoint = marginsL[index + 1]
+			if nextPoint[0] > (actualPoint[0] + valueFreq) and nextPoint[1] > actualPoint[1]:
+				marginsL[index + 1][0] = actualPoint[0] + valueFreq
+			# right
+			actualPoint = marginsR[index]
+			nextPoint = marginsR[index + 1]
+			if nextPoint[0] < (actualPoint[0] - valueFreq) and nextPoint[1] > actualPoint[1]:
+				marginsR[index + 1][0] = actualPoint[0] - valueFreq
+
+			# left
+			actualPoint = marginsL[total - index]
+			nextPoint = marginsL[total - index - 1]
+			if nextPoint[0] > (actualPoint[0] + valueFreq) and nextPoint[1] < actualPoint[1]:
+				marginsL[total - index - 1][0] = actualPoint[0] + valueFreq
+			# right
+			actualPoint = marginsR[total - index]
+			nextPoint = marginsR[total - index - 1]
+			if nextPoint[0] < (actualPoint[0] - valueFreq) and nextPoint[1] < actualPoint[1]:
+				marginsR[total - index - 1][0] = actualPoint[0] - valueFreq
+
+		return marginsL, marginsR
+
+	# close counterforms, creating a polygon
+	def closeOpenCounters(self, marginsL, marginsR, puntosExtremos):
+		initPoint = puntosExtremos[0][0], self.minYref
+		endPoint = puntosExtremos[0][0], self.maxYref
+		marginsL.insert(0, initPoint)
+		marginsL.append(endPoint)
+
+		initPoint = puntosExtremos[1][0], self.minYref
+		endPoint = puntosExtremos[1][0], self.maxYref
+		marginsR.insert(0, initPoint)
+		marginsR.append(endPoint)
+		return marginsL, marginsR
+
+	def italizePoint(self, p):
+		if p[0] is not None:
+			px = p[0]
+			py = p[1]
+			cateto = self.mline - p[1]
+			xvar = -rectCateto(self.angle, cateto)
+			p = [px + xvar, py]
+		return p
+
+	def deItalizePoint(self, p):
+		if p[0] is not None:
+			px = p[0]
+			py = p[1]
+			cateto = p[1] - self.mline
+			xvar = -rectCateto(self.angle, cateto)
+			p = [px + xvar, py]
+		return p
+
+	def deSlant(self, margins):
+		if self.angle > 0:
+			for index in range(len(margins[0])):
+				margins[0][index] = self.deItalizePoint(margins[0][index])
+				margins[1][index] = self.deItalizePoint(margins[1][index])
+		return margins
+
+	def slant(self, margins):
+		for index in range(len(margins[0])):
+			margins[0][index] = self.italizePoint(margins[0][index])
+			margins[1][index] = self.italizePoint(margins[1][index])
+		return margins
+
+	def calcularValorSb(self, poligono):
+		amplitudeY = self.maxYref - self.minYref
+		# calculates proportional area
+		areaProporcional = (amplitudeY * self.area) / self.xHeight
+		areaPoligono = area(poligono)
+		valor = areaProporcional - areaPoligono
+		nuevoValor = valor / amplitudeY
+		return nuevoValor
+
+	def setSpace(self, layer, referenceLayer):
+		
+		self.layer = layer
+		self.area = self.paramArea * self.factor * 100
+
+		# get reference glyph maximum points
+		bounds = referenceLayer.bounds
+		valorExcedente = self.excedente()
+
+		# store min and max y
+		self.minYref = NSMinY(bounds) - valorExcedente
+		self.maxYref = NSMaxY(bounds) + valorExcedente
+
+		# bounds
+		bounds = self.layer.bounds
+
+		# all the margins
+		marginsOne = marginList(self.layer, NSMinY(bounds), NSMaxY(bounds))
+
+		# creates a list with left and right margins
+		marginsFull = [marginsOne[0], marginsOne[1]]
+
+		# filter values between min and max
+		margins = marginsZone(marginsFull, self.minYref, self.maxYref)
+
+		# create a closed polygon
+		poligonos = self.processMargins(margins)
+
+		# get area from each side
+		areaL = area(poligonos[0])
+		areaR = area(poligonos[1])
+
+		# deitalize margins
+		marginsFull = self.deSlant(marginsFull)
+		# get extreme points deitalized
+		extremosFull = self.maxPoints(marginsFull[0] + marginsFull[1], NSMinY(bounds), NSMaxY(bounds))
+		# get zone extreme points
+		extremos = self.maxPoints(margins[0] + margins[1], self.minYref, self.maxYref)
+
+		# dif between extremes full and zone
+		distanciaL = math.ceil(extremos[0][0] - extremosFull[0][0])
+		distanciaR = math.ceil(extremosFull[1][0] - extremos[1][0])
+
+		# set new sidebearings
+		self.newL = math.ceil(0 - distanciaL + self.calcularValorSb(poligonos[0]))
+		self.newR = math.ceil(0 - distanciaR + self.calcularValorSb(poligonos[1]))
+
+		# tabVersion
+		if '.tosf' in layer.parent.name or '.tf' in layer.parent.name or self.tab or self.tabVersion:
+			if not window:
+				self.ancho = self.layer.width
+			anchoForma = extremosFull[1][0] - extremosFull[0][0]
+			anchoActual = anchoForma + self.newL + self.newR
+			anchoDiff = (self.ancho - anchoActual) / 2
+
+			self.newL += anchoDiff
+			self.newR += anchoDiff
+			self.newWidth = self.ancho
+
+			self.output += layer.parent.name + ' Fue ajustado tabularmente a ' + str(self.ancho)
+		# fin tabVersion
+
+		# if there is a metric rule
+		else:
+			if layer.parent.leftMetricsKey is not None or self.LSB == False:
+				self.newL = self.layer.LSB
+
+			if layer.parent.rightMetricsKey is not None or self.RSB == False:
+				self.newR = self.layer.RSB
+
+	def spaceMain(self, layer, referenceLayer):
+		try:
+			self.output = ""
+			if not layer.name:
+				self.output += 'Hay algo raro... Probablemente un salto de linea'
+			elif len(layer.paths) < 1 and len(layer.components) < 1:
+				self.output += 'Glyph ' + layer.parent.name + " hasn't any path\n"
+			# both sidebearings with metric keys
+			elif layer.hasAlignedWidth():
+				self.output += 'Glifo ' + layer.parent.name + ' has automatic alignment.\n'
+			elif layer.parent.leftMetricsKey is not None and layer.parent.rightMetricsKey is not None:
+				self.output += 'Glifo ' + layer.parent.name + " has metric keys. Don't forget to update it.\n"
+			# if it is tabular
+			# elif '.tosf' in layer.parent.name or '.tf' in layer.parent.name:
+				#self.output+='Glifo '+layer.parent.name +' se supone tabular..'+"\n"
+			# if it is fraction
+			elif 'fraction' in layer.parent.name:
+				self.output += 'Glifo ' + layer.parent.name + ': should be checked and done manually.\n'
+			# if not...
+			else:
+				self.setSpace(layer, referenceLayer)
+				# store values in a list
+				setSidebearings(layer, self.newL, self.newR, self.newWidth, color)
+			
+			print self.output
+			self.output = ''
+			
+		# traceback
+		except Exception as ex:
+			import traceback
+			print traceback.format_exc()
