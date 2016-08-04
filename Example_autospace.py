4e38162eb75328702db2e583245eb46013bca2fb
schriftgestalt <georg.seifert@mac.com>
2016-08-04 16:35:50 +0200

    initial commit

---
+++ Example_autospace.py
@@ -0,0 +1,41 @@
+#CATEGORIAS
+#Script, Category, Subcategory, value, referenceGlyph, filter
+#declarar de general a particular
+*,Letter,Uppercase,1.25,H,*,
+*,Letter,Smallcaps,1.1,h.sc,*,
+*,Letter,Lowercase,1,x,*,
+#*,Letter,Lowercase,0.7,x.sups,.sups,
+*,Letter,Superscript,0.7,ordfeminine,*,
+
+#numeros
+*,Number,Decimal Digit,1.3,one,*,
+*,Number,Fraction,1.3,*,*,
+*,Number,*,0.8,*,.dnom,
+*,Number,*,0.8,*,.numr,
+*,Number,*,0.8,*,.inferior,
+*,Number,*,0.8,*,superior,
+
+#puntuacionyotros
+#*,Punctuation,*,1.5,*,quote,
+*,Punctuation,Other,1.4,*,*,
+*,Punctuation,Parenthesis,1.5,*,*,
+*,Punctuation,Quote,1.2,*,*,
+*,Punctuation,Quote,1.35	,*,guil,
+*,Punctuation,Dash,1,*,*,
+*,Punctuation,*,1,*,slash,
+*,Punctuation,*,1.2,*,*,
+
+
+*,Symbol,Currency,1.6,*,*,
+*,Symbol,Math,2,*,*,
+#*,Symbol,Math,1.7,*,per,#percent
+*,Symbol,*,1.5,*,*,
+*,Mark,*,1,*,*,
+
+#deva
+devanagari,Letter,Other,1,devaHeight,*,
+devanagari,Letter,Ligature,1,devaHeight,*,
+devanagari,Letter,Matra,1,devaHeight,*,
+devanagari,Letter,Halfform,1,devaHeight,*,
+devanagari,Number,Decimaldigit,1,devaHeight,*,
+devanagari,Mark,Nonspacing,1,devaHeight,*,
