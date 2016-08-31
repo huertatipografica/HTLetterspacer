736be60918fb6e389d7e0fbbc958b3b69f0b6c4d
juandelperal <juandelperal@gmail.com>
2016-08-31 15:58:19 -0300

    added content

--- docs/index.php
+++ docs/index.php
@@ -15,26 +15,20 @@
     <div class="row">
       <div class="large-12 columns text-center">
         <ul class="horizontal menu" data-magellan>
+          <li>
+            <a target="_blank" href="http://www.huertatipografica.com/">
+              <img src="/images/ht.png" alt="Huerta Tipográfica">
+            </a>
+          </li>
           <li><a href="#download">Download</a></li>
           <li><a href="#start">Get started</a></li>
-          <li><a href="#idea">Fundamentals</a></li>
+          <li><a href="#examples">Examples</a></li>
+          <li><a href="#idea">Fundaments</a></li>
           <li><a href="#contribute">Contribute</a></li>
         </ul>
       </div>
     </div>
   </header>
-
-  <div data-sticky-container>
-  <div class="sticky" id="example" data-sticky data-margin-top="0" style="width:100%;" data-margin-bottom="0" data-top-anchor="topAnchorExample" data-btm-anchor="bottomOfContentId:bottom">
-    <nav data-magellan>
-      <ul class="horizontal menu expanded">
-      <li><a href="#first">First Arrival</a></li>
-      <li><a href="#second">Second Arrival</a></li>
-      <li><a href="#third">Third Arrival</a></li>
-      </ul>
-    </nav>
-  </div>
-</div>
 
 
 
@@ -56,9 +50,12 @@
 
     <section id="download" data-magellan-target="download">
       <div class="row">
-        <div class="large-8 large-offset-3  columns">
+        <div class="large-7 large-offset-3 medium-8 medium-offset-2  columns">
 
-            <p class="lead">Alguien que hable bien del proyecto o algún párrafo marketinero: ej. yo perdí peso usando Reduce Fat Fast. Era un infeliz, pero ahora todas las mujeres se pelan por estar a mi lado.</p>
+          <p>
+          <img src="/images/q.png">
+          Alguien (o un slider de citas) que hable bien del proyecto o algún párrafo marketinero: ej. yo perdí peso usando Reduce Fat Fast. Era un infeliz, pero ahora todas las mujeres se pelan por estar a mi lado.
+          </p>
 
           <a href="#" class="button success"><i class="fa fa-download"></i> Download</a>
           <a href="#" class="button secondary"><i class="fa fa-github"></i> Contribute</a>
@@ -90,7 +87,7 @@
             <h2>Contribute</h2>
             <p>
               The Huerta Tipográfica Letter Spacer is an open source tool. You can contribute on <a href="https://github.com/huertatipografica"> our repo in Github</a>.
-              </p>
+            </p>
           </div>
         </div>
       </section>
@@ -99,10 +96,16 @@
 
 
 
-<script src="/js/jquery.min.js"></script>
-<script src="/js/what-input.min.js"></script>
-<script src="/js/foundation.min.js"></script>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/what-input.min.js"></script>
+    <script src="/js/foundation.min.js"></script>
 
-<script src="/js/app.js"></script>
-</body>
-</html>
+    <script src="/js/app.js"></script>
+
+
+
+    <?php
+    include 'docs.php';
+     ?>
+  </body>
+  </html>
---
+++ todo.md
@@ -0,0 +1,16 @@
+DOWNLOAD
+GET STARTED
+	What do I need to set up to use it?
+	Parameters
+	Configuration file
+EXAMPLES
+	Can I see it in action?
+	Can I see example fonts spaced with the tool?
+FUNDAMENTS
+	So it replaces the type designer's craft?
+	What are the ideas behind it?
+	Project History
+CONTACT
+
+
+
