d194f1e8c80b221c28f20ed8913c6b0b6d4d2f6d
juandelperal <juandelperal@gmail.com>
2016-09-13 20:39:33 -0300

    get rid of php

--- parts/contribute.php
+++
@@ -1,9 +0,0 @@
-<section id="contribute" data-magellan-target="contribute">
-	<div class="row">
-  		<div class="medium-8 medium-offset-3 columns">
-			<h2>Contribute</h2>
-			<p>We want this tool to be open and free to the community, to receive feedback and improvements made by other designers. You can contribute on <a href="https://github.com/huertatipografica/HTLetterspacer"> our repo in Github</a>.
-			</p>
-		</div>
-	</div>
-</section>
