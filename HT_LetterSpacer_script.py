b1b08cb16da3170269c03be84becb8fc5a16f19c
jbmorizot <jbmorizot@users.noreply.github.com>
2022-03-23 17:54:52 +0100

    Remove print

--- HT_LetterSpacer_script.py
+++ HT_LetterSpacer_script.py
@@ -652,16 +652,15 @@ class HTLetterspacerScript(object):
 		exception = False
 		if self.case > 0:
 			if self.case == 1:
-				self.category = "Uppercase"
+				self.subCategory = "Uppercase"
 			elif self.case == 2:
-				self.category = "Lowercase"
+				self.subCategory = "Lowercase"
 		for item in self.config:
 			if self.script == item[0] or item[0] == '*':
 				if self.category == item[1] or item[1] == '*':
 					if self.subCategory == item[2] or item[2] == '*':
 						if not exception or item[5] in self.glyph.name:
 							exception = item
-		print(exception, self.case, item[2])
 		return exception
 
 	def setG(self, layer):
